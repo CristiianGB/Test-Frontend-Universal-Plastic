5f16e286a4683faa05c4055c38fe3ae7a7056446
Cristian Guardeño <cristian.guardenyo@gmail.com>
2023-08-12 20:37:34 +0200

    all feats added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/navbarStyles.css"
 import locationIcon from "../assets/locationIcon.svg"
@@ -10,6 +10,11 @@ import {
 } from "@/components/ui/navigation-menu"
 
 const Navbar = () => {
+    const [activeLocation, setActiveLocation] = useState(true)
+    const [activeWeather, setActiveWeather] = useState(false)
+    const unactive = "opacity-25"
+
+
     return (
         <>
             <div className="container-fluid">
@@ -17,13 +22,25 @@ const Navbar = () => {
                     <div className="col-12 d-flex justify-content-center">
                         <NavigationMenu>
                             <NavigationMenuList>
-                                <NavigationMenuItem className="mx-4">
+                                <NavigationMenuItem 
+                                onClick={()=>{
+                                    setActiveLocation(true)
+                                    setActiveWeather(false)
+                                }} 
+                                className={`mx-4 ${activeLocation && !activeWeather ? "" : unactive}`}
+                                >
                                     <Link to="/">
                                             <img src={locationIcon} alt="location icon" />
                                             <tool-tip role="tool-tip">Location</tool-tip>
                                     </Link>
                                 </NavigationMenuItem>
-                                <NavigationMenuItem className="mx-4">
+                                <NavigationMenuItem 
+                                onClick={()=>{
+                                    setActiveLocation(false)
+                                    setActiveWeather(true)
+                                }} 
+                                className={`mx-4 ${activeWeather && !activeLocation ? "" : unactive}`}
+                                >
                                     <Link to="/weather-city">
                                             <img src={weatherIcon} alt="weather icon" />
                                             <tool-tip role="tool-tip">Weather</tool-tip>
